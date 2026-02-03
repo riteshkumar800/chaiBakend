700ab816341d9777095490f4c0c7f6f9ef7b3ee0
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-02-03 23:34:23 +0530

    code add

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ app.get('/', (req,res)=>{
     res.send('Hello world')
 })
 
+app.get('/twitter', (req,res)=>{
+    res.send('riteshdotcom')
+})
 
+app.get('/login', (req,res)=>{
+    res.send('<h1>please login at chai aur code</h1>')
+})
+app.get('/youtube', (req,res)=>{
+    res.send('<h2>chai aur code</h2>')
+})
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
