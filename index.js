eb85bb3616a849df4a8fa320dc75a56840c0094d
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-02-03 23:28:30 +0530

    add

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express = require('express')
+// import express from "express"
+const app= express()
+const port=3000
+
+app.get('/', (req,res)=>{
+    res.send('Hello world')
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
