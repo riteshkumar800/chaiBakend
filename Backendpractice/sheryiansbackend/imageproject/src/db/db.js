bc40a4b1f73245ec9e9f999e90ceba79db1cc4af
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-02-13 04:05:56 +0530

    updated

--- Backendpractice/sheryiansbackend/imageproject/src/db/db.js
+++ Backendpractice/sheryiansbackend/imageproject/src/db/db.js
@@ -4,5 +4,7 @@ async function connectDB(){
     await mongoose.connect("mongodb+srv://yt-backend:PwHPiGym6dlTfE7a@yt-backend.pxeroak.mongodb.net/")
     console.log("connected to DB")
 
-    
+
 }
+
+module.exports = connectDB
---
+++ Backendpractice/sheryiansbackend/imageproject/src/models/post.models.js
@@ -0,0 +1,10 @@
+const mongoose= require("mongoose")
+
+
+const postSchema= new mongoose.Schema({
+    image:String,
+    caption:string,
+})
+
+
+const postModel = mongoose.model("post", postSchema)
