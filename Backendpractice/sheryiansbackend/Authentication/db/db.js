c17821deb245dbec6704450025839def0e2af503
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-07 00:43:45 +0530

    new

--- Backendpractice/sheryiansbackend/Authentication/db/db.js
+++ Backendpractice/sheryiansbackend/Authentication/db/db.js
@@ -3,6 +3,11 @@ const mongoose=require('mongoose')
 
 async function connectDB(){
     try {
-        await 
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected successfully");
+    } catch (error){
+        console.error("Database connection rror:", err);
     }
 }
+
+module.exports = connectDB;
