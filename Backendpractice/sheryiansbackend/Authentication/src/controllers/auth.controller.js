0fc37bd88fc62c55ea287d82a77c41a6de7db1d5
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-10 01:45:33 +0530

    added

--- Backendpractice/sheryiansbackend/Authentication/src/controllers/auth.controller.js
+++ Backendpractice/sheryiansbackend/Authentication/src/controllers/auth.controller.js
@@ -1,12 +1,18 @@
 const userModel = require ("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 
 
 async function registerUser(req,res){
     const {username, email, password} = req.body;
 
+        const hash = await bcrypt.hash(password,10);
 
 
-    const user=userModel.create({
+
+
+    const user=await userModel.create({
         username, email, password
     })
 
