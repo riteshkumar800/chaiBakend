0ef5634f6a5cdc45ec69e6b04884e1e065d26015
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-09 23:36:37 +0530

    added

--- Backendpractice/sheryiansbackend/Authentication/src/controllers/auth.controller.js
+++ Backendpractice/sheryiansbackend/Authentication/src/controllers/auth.controller.js
@@ -4,7 +4,18 @@ const userModel = require ("../models/user.model");
 async function registerUser(req,res){
     const {username, email, password} = req.body;
 
+
+
+    const isUserAlreadyExists=await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+
 }
+
+
 
 module.exports={registerUser}
 
