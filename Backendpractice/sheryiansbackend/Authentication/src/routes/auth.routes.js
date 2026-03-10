aaa3c39ebb85e98432bb082b3a0262b4d27886cf
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-11 01:57:54 +0530

    added

--- Backendpractice/sheryiansbackend/Authentication/src/routes/auth.routes.js
+++ Backendpractice/sheryiansbackend/Authentication/src/routes/auth.routes.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const authController=require("../controllers/auth.controller")
+const authController = require("../controllers/auth.controller");
 
+const router = express.Router();
 
-const router=express.Router();
+router.post("/register", authController.registerUser);
 
+router.get("/test", (req, res) => {
+    console.log("Cookies:", req.cookies);
 
-router.post("/register", authController.registerUser)
-router.get("/test", (req,res)=>{
-    console.log("Cookies: ", req.cookies)
     res.json({
         message: "Test route",
         cookies: req.cookies
-    })
-})
+    });
+});
 
-module.exports=router;
-
+module.exports = router;
