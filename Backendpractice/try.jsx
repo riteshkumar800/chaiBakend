8479807336c71ea0e2b9b55148c3231e6a05bf5b
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-05-23 21:56:51 +0530

    added

--- Backendpractice/try.jsx
+++ Backendpractice/try.jsx
@@ -1,46 +1,46 @@
-import React from 'react'
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
+// import React from 'react'
+// import axios from "axios"
+// import { useNavigate } from "react-router-dom"
 
 
-const CreatePost = () => {
+// const CreatePost = () => {
 
-    const navigate = useNavigate()
+//     const navigate = useNavigate()
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-
-        const formData = new FormData(e.target)
-
-        axios.post("http://localhost:3000/create-post", formData)
-            .then((res) => {
-
-                navigate("/feed")
-
-            })
-            .catch((err) => {
-                console.log(err)
-                alert("Error creating post")
-            })
+//     const handleSubmit = async (e) => {
+//         e.preventDefault()
 
 
-    }
+//         const formData = new FormData(e.target)
 
-    return (
-        <section className='create-post-section' >
-            <h1>Create post</h1>
+//         axios.post("http://localhost:3000/create-post", formData)
+//             .then((res) => {
 
-            <form onSubmit={handleSubmit} >
+//                 navigate("/feed")
 
-                <input type="file" name="image" accept="image/*" />
-                <input type="text" name='caption' placeholder='Enter caption' required />
-                <button type='submit' >Submit</button>
+//             })
+//             .catch((err) => {
+//                 console.log(err)
+//                 alert("Error creating post")
+//             })
 
-            </form>
 
-        </section>
-    )
-}
+//     }
 
-export default CreatePost
+//     return (
+//         <section className='create-post-section' >
+//             <h1>Create post</h1>
+
+//             <form onSubmit={handleSubmit} >
+
+//                 <input type="file" name="image" accept="image/*" />
+//                 <input type="text" name='caption' placeholder='Enter caption' required />
+//                 <button type='submit' >Submit</button>
+
+//             </form>
+
+//         </section>
+//     )
+// }
+
+// export default CreatePost
